6eb4197145bd8887d3ac01ddfdc81ba8715efd2f
sophysophysophysophy <61459@naver.com>
2022-05-25 18:25:30 +0900

    fin

--- advanced/src/main/java/hello/advanced/app/v5/OrderRepositoryV5.java
+++ advanced/src/main/java/hello/advanced/app/v5/OrderRepositoryV5.java
@@ -2,8 +2,6 @@ package hello.advanced.app.v5;
 
 import hello.advanced.trace.callback.TraceTemplate;
 import hello.advanced.trace.logtrace.LogTrace;
-import hello.advanced.trace.template.AbstractTemplate;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
